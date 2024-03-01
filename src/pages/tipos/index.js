5a0dc515bd29715c3044a3121ec164d77fa7f39f
Matheus Jesus dos Santos <Matheus.Jesus@fesfsus.ba>
2024-03-01 09:18:30 -0300

    Adiciona navbar e rota para a pagina de tipos

--- src/pages/tipos/index.js
+++ src/pages/tipos/index.js
@@ -1,5 +1,6 @@
 "use client";
 
+import Navbar from "@/app/components/Navbar";
 import "../../app/globals.css";
 import "flowbite";
 import { useState, useEffect } from "react";
@@ -74,127 +75,136 @@ export default function Tipos() {
   };
 
   return (
-    <div className="container mx-auto mt-6">
-      <button
-        id="dropdownDefaultButton"
-        data-dropdown-toggle="dropdown"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        type="button"
-        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-      >
-        {selectedType ? selectedType.name : "Tipos de Pokemons"}
-        <svg
-          className="w-2.5 h-2.5 ms-3"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 10 6"
+    <>
+      <Navbar />
+      
+      <div className="container mx-auto mt-6">
+        <button
+          id="dropdownDefaultButton"
+          data-dropdown-toggle="dropdown"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          type="button"
+          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
         >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="m1 1 4 4 4-4"
-          />
-        </svg>
-      </button>
+          {selectedType ? selectedType.name : "Tipos de Pokemons"}
+          <svg
+            className="w-2.5 h-2.5 ms-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 10 6"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m1 1 4 4 4-4"
+            />
+          </svg>
+        </button>
 
-      <div
-        id="dropdown"
-        className={`z-10 ${
-          isDropdownOpen ? "" : "hidden"
-        } bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 absolute left-0 mt-2`}
-      >
-        <ul
-          className="py-2 text-sm text-gray-700 dark:text-gray-200"
-          aria-labelledby="dropdownDefaultButton"
+        <div
+          id="dropdown"
+          className={`z-10 ${
+            isDropdownOpen ? "" : "hidden"
+          } bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 absolute left-0 mt-2`}
         >
-          {typePokemon &&
-            typePokemon.map((type, index) => (
-              <li key={index}>
-                <a
-                  href="#"
-                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                  onClick={() => handleTypeClick(type)}
-                >
-                  {type.name}
-                </a>
-              </li>
-            ))}
-        </ul>
-      </div>
-
-      <button
-        onClick={handleSearch}
-        className="ml-4 text-white end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Buscar
-      </button>
-
-      <div className="mt-4">
-        {console.log("pokemonList:", pokemonList)}
-
-        {pokemonList && (
-          <div className="mt-6 relative overflow-x-auto">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                  <th scope="col" className="px-6 py-3">
-                    Sprite
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    ID
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Name
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Height
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Weight
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Type1
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Type2
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Base Experience
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {pokemonList.map((pokemon, index) => (
-                  <tr
-                    key={index}
-                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+          <ul
+            className="py-2 text-sm text-gray-700 dark:text-gray-200"
+            aria-labelledby="dropdownDefaultButton"
+          >
+            {typePokemon &&
+              typePokemon.map((type, index) => (
+                <li key={index}>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    onClick={() => handleTypeClick(type)}
                   >
-                    <td className="px-6 py-4"><img src={pokemon.sprites.front_default} alt="Pokemon"></img></td>
-                    <td className="px-6 py-4">{pokemon.id}</td>
-                    <td className="px-6 py-4">{pokemon.name}</td>
-                    <td className="px-6 py-4">{pokemon.height}</td>
-                    <td className="px-6 py-4">{pokemon.weight}</td>
-                    <td className="px-6 py-4">
-                      {pokemon.types && pokemon.types.length > 0
-                        ? pokemon.types[0]?.type?.name
-                        : "N/A"}
-                    </td>
-                    <td className="px-6 py-4">
-                      {pokemon.types && pokemon.types.length > 1
-                        ? pokemon.types[1]?.type?.name
-                        : "N/A"}
-                    </td>
-                    <td className="px-6 py-4">{pokemon.base_experience}</td>
+                    {type.name}
+                  </a>
+                </li>
+              ))}
+          </ul>
+        </div>
+
+        <button
+          onClick={handleSearch}
+          className="ml-4 text-white end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Buscar
+        </button>
+
+        <div className="mt-4">
+          {console.log("pokemonList:", pokemonList)}
+
+          {pokemonList && (
+            <div className="mt-6 relative overflow-x-auto">
+              <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                    <th scope="col" className="px-6 py-3">
+                      Sprite
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      ID
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Name
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Height
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Weight
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Type1
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Type2
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Base Experience
+                    </th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        )}
+                </thead>
+                <tbody>
+                  {pokemonList.map((pokemon, index) => (
+                    <tr
+                      key={index}
+                      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                    >
+                      <td className="px-6 py-4">
+                        <img
+                          src={pokemon.sprites.front_default}
+                          alt="Pokemon"
+                        ></img>
+                      </td>
+                      <td className="px-6 py-4">{pokemon.id}</td>
+                      <td className="px-6 py-4">{pokemon.name}</td>
+                      <td className="px-6 py-4">{pokemon.height}</td>
+                      <td className="px-6 py-4">{pokemon.weight}</td>
+                      <td className="px-6 py-4">
+                        {pokemon.types && pokemon.types.length > 0
+                          ? pokemon.types[0]?.type?.name
+                          : "N/A"}
+                      </td>
+                      <td className="px-6 py-4">
+                        {pokemon.types && pokemon.types.length > 1
+                          ? pokemon.types[1]?.type?.name
+                          : "N/A"}
+                      </td>
+                      <td className="px-6 py-4">{pokemon.base_experience}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
